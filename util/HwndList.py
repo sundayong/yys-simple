dcd1e2bf37cb26f118b477047509ae5e37fec8c1
sundayong <1137353877@qq.com>
2020-03-14 11:21:18 +0800

    修改

---
+++ util/HwndList.py
@@ -0,0 +1,20 @@
+import win32gui
+
+hwnd_title = dict()
+
+def get_all_hwnd(hwnd, mouse):
+    if win32gui.IsWindow(hwnd) and win32gui.IsWindowEnabled(hwnd) and win32gui.IsWindowVisible(hwnd):
+        hwnd_title.update({hwnd: win32gui.GetWindowText(hwnd)})
+
+def get():
+    list = [];
+    win32gui.EnumWindows(get_all_hwnd, 0)
+    for h, t in hwnd_title.items():
+        if t is not "":
+            if t == '阴阳师-网易游戏':
+                list.append(h)
+                print(h)
+    return list;
+
+if __name__ == '__main__':
+    get()
